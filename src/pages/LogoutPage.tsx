7f16ca0e408bc4b8c185813e1c2a914a5a88b872
Kim McCall <mccall.kim@gmail.com>
2025-09-02 09:28:47 -0700

    Nice usage of useEffect to delay final rendering

--- src/pages/LogoutPage.tsx
+++ src/pages/LogoutPage.tsx
@@ -1,26 +1,33 @@
-import { useEffect } from "react";
+import { useState, useEffect, SetStateAction } from "react";
 import { useNavigate } from 'react-router-dom';
 import { signOut } from 'aws-amplify/auth';
 
 import '@aws-amplify/ui-react/styles.css'; // Import default styles
 
-async function handleSignOut() {
+async function handleSignOut(setSignedOut: { (value: SetStateAction<boolean>): void; (arg0: boolean): void; }) {
   try {
     await signOut();
     console.log('User signed out successfully.');
+    setSignedOut(true);
   } catch (error) {
     console.error('Error signing out:', error);
   }
 }
 
 function LogoutPage() {
+  const [signedOut, setSignedOut] = useState(false);
   const navigate = useNavigate();
 
-  handleSignOut();
+  handleSignOut(setSignedOut);
 
   useEffect(() => {
-    navigate("/", { replace: true });
-  }, []);
+    if (signedOut) {
+      console.log("in LogoutPage, signedOut is true; navigating to /");
+      navigate("/", { replace: true });
+    } else {
+      console.log("in LogoutPage, signedOut is false; staying here");
+    }
+  }, [navigate, signedOut]);
   
   return (
     <>
