c6199249cf86ca2796bf68c318fddc5002d7147c
Kim McCall <mccall.kim@gmail.com>
2025-09-08 14:13:11 -0700

    Fix some build breakage

--- src/components/SuggestionTile.tsx
+++ src/components/SuggestionTile.tsx
@@ -1,28 +1,19 @@
 import './SuggestionTile.css';
 
-/*
-const bar: React.CSSProperties = {
-  height: '730px',
-  width: '140px',
-  border: '1px solid blue',
-  padding: '6px',
-  margin: '1px',
-  backgroundColor: '#d0e0ff',
-};
+interface MyProps {
+  key: string;
+  suggestionId?: string;
+}
 
-const buttonDiv: React.CSSProperties = {
-  cursor: 'pointer',
-  fontWeight: 'bold',
-  fontSize: '150%',
-};
-*/
 const handleButtonClick = (suggestionId: string) => {
   console.log(`Should now bring up editor for suggestion with ID ${suggestionId}`)
 }
 
-function SuggestionTile({ suggestionId }) {
+const SuggestionTile: React.FC<MyProps> = (props)  => {
+  const { suggestionId } = props;
+  const  definedSuggestionId = suggestionId || '';
   return (
-    <div className='tile' onClick={() => {handleButtonClick(suggestionId)}} >
+    <div className='tile' onClick={() => {handleButtonClick(definedSuggestionId)}} >
       Partial content of suggestion with ID: {String(suggestionId)}
     </div>
   );
