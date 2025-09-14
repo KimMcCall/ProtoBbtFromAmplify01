fedf622ec4d839d26c1cab464d816017ac6d0941
Kim McCall <mccall.kim@gmail.com>
2025-09-14 04:42:00 -0700

    Introduce (stub of) AdminIssuesPage

---
+++ src/pages/AdminIssuesPage.tsx
@@ -0,0 +1,13 @@
+import PageWrapper from "../components/PageWrapper";
+
+function AdminIssuesPage() {
+  return (
+    <PageWrapper>
+      <div>
+        You're on the (stub of the) Admin Issues Page.
+      </div>
+    </PageWrapper>
+  );
+}
+
+export default AdminIssuesPage;
