40b5d5b4e99cd63e4498f7b46ea1e9bf248dd870
Kim McCall <mccall.kim@gmail.com>
2025-09-20 20:56:05 -0700

    Got the UI working nicely for changing non-URL properies of Issues

--- src/pages/PlayPage02.tsx
+++ src/pages/PlayPage02.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { ChangeEvent, SyntheticEvent, useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import { Button, Flex, TextField } from "@aws-amplify/ui-react";
+import { Button, Flex, Radio, RadioGroupField, TextField } from "@aws-amplify/ui-react";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
 import PageWrapper from "../components/PageWrapper";
 import { selectCurrentUserIsLoggedIn, selectCurrentUserIsSuperAdmin, setCurrentUserAsSuperAdmin, setCurrentUserCanonicalEmail } from "../features/userInfo/userInfoSlice";
@@ -13,6 +13,34 @@ import ToastNotifier from "../components/ToastNotifier";
 import { createIssue } from "../utils/dynamodb_operations";
 import { getIssue } from "../utils/comment_operations";
 import { defaultConAuthor, defaultProAuthor, ProxyForNoUrl } from "../utils/constants";
+import './PlayPage02.css';
+import { IssueType, selectAllIssues } from "../features/issues/issues";
+
+interface ImageTilePropsType {
+  issueId: string
+  claim: string
+  selectionCallback: (e: SyntheticEvent<HTMLDivElement>, issueId: string) => void
+}
+
+interface IdClaimPairType {
+  issueId: string
+  claim: string
+}
+
+function IssueTile(props: ImageTilePropsType) {
+  const { issueId, claim, selectionCallback } = props;
+
+  const handleTileClick = (event: SyntheticEvent<HTMLDivElement>) => {
+    event.stopPropagation();
+    selectionCallback(event, issueId);
+  }
+
+  return(
+    <div key={issueId} className="issueTileRoot" onClick={handleTileClick}>
+      {claim}
+    </div>
+  )
+}
 
 function PlayPage02() {
   const [ savedPath, setSavedPath] = useState("/donate");
@@ -20,10 +48,30 @@ function PlayPage02() {
   const [ dbCheckFeedback, setDbCheckFeedback ] = useState("Waiting for czech");
   const [simpleTestResult, setSimpleTestResult] = useState(42)
   const [showToast, setShowToast] = useState(false);
+  const [uiChoice, setUIChoice] = useState('uiMgmt');
+  const [activityChoice, setActivityChoice] = useState('manageOtherFields');
+  const [issueIdText, setIssueIdText] = useState('');
+  const [claimText, setClaimText] = useState('');
+  const [priorityText, setPriorityText] = useState('');
+  const [availabilityChoice, setAvailabilityChoice] = useState('unavailable');
+
   const dispatch = useAppDispatch();
   const isNowSuperAdmin = useAppSelector(selectCurrentUserIsSuperAdmin);
   const newPath = useAppSelector(selecNext);
   const navigateTo = useNavigate();
+
+  const allIssues: IssueType[] = useAppSelector(selectAllIssues);
+  const issueMap = new Map();
+  allIssues.forEach((issue) => issueMap.set(issue.issueId, issue.claim))
+  let idClaimPairs: IdClaimPairType[] = [];
+  issueMap.forEach((value, key) => {
+    const issueId = key;
+    const claim = value;
+    const struct = { issueId: issueId, claim: claim };
+    idClaimPairs = idClaimPairs.concat(struct);
+  });
+  console.log(`# idClaimPairs: ${idClaimPairs.length}`)
+  console.log(idClaimPairs);
 
   const toggleIsAdmin = () => {
     dispatch(setCurrentUserAsSuperAdmin(!isNowSuperAdmin))
@@ -230,68 +278,254 @@ function PlayPage02() {
   const clearOrFill = isLoggedIn ? 'Clear' : 'Fill';
   const toastMessage = "This is a long enough text to stretch across multiple lines, I hope";
 
+  const handleUiRadioButtonChange = (event: ChangeEvent<HTMLInputElement>) => {
+    event.stopPropagation();
+    const choice = event.target.value;
+    console.log(`choice: '${choice}.`)
+    setUIChoice(choice)
+  }
+
+  const handleActivityRadioButtonChange = (event: ChangeEvent<HTMLInputElement>) => {
+    event.stopPropagation();
+    const choice = event.target.value;
+    console.log(`activity choice: '${choice}.`)
+    setActivityChoice(choice)
+  }
+
+  const handleAvailabilityRadioButtonChange = (event: ChangeEvent<HTMLInputElement>) => {
+    event.stopPropagation();
+    const choice = event.target.value;
+    console.log(`availabillity choice: '${choice}'.`)
+    setAvailabilityChoice(choice)
+  }
+
+  const handleOtherFieldsClearButtonClick = (event: SyntheticEvent<HTMLButtonElement>) => {
+    event.stopPropagation();
+    setIssueIdText('');
+    setClaimText('');
+    setPriorityText('');
+    setAvailabilityChoice('unavailable')
+  }
+
+  const handleOtherFieldsSubmitButtonClick = (event: SyntheticEvent<HTMLButtonElement>) => {
+    event.stopPropagation();
+    console.log("Should now update the DB")
+  }
+
+  const handleIssueTileClick = (event: SyntheticEvent<HTMLDivElement>, issueId: string) => {
+    event.stopPropagation();
+    setIssueIdText(issueId);
+  }
+
+  const showUiMgmt = uiChoice === 'uiMgmt';
+  const showRandom = uiChoice === 'randomUI';
+  const showUrlUi = activityChoice === 'manageUrls';
+  const showOtherFieldsUi = activityChoice === 'manageOtherFields';
+
   return (
     <PageWrapper>
-      <Flex direction="column">
-        <Flex direction={"row"}>
-          <button onClick={() => toggleIsAdmin()}>Toggle isSuperAdmin</button>
-          value:
-          <TextField
-            label=""
-            value={'' + isNowSuperAdmin}
-            readOnly
-            placeholder="??"
-            width="120px"
-          />
-        </Flex>
-        <Flex direction={"row"}>
-          <button onClick={() => toggleCEmail()}>{String(clearOrFill)} cEmail</button>
-          <button onClick={() => checkDbAndShowResult()}>Czech DB for corruption</button>
-          <TextField
-            label=""
-            value={dbCheckFeedback}
-            readOnly
-            width="420px"
-          />
-        </Flex>
-        <Flex direction={"row"}>
-          <button onClick={() => dispatch(setNextPath(savedPath))}>Set as next path</button>
-          <TextField
-            label=""
-            value={savedPath}
-            onChange={(e) => setSavedPath(e.target.value)}
-            width="200px"
-          />
-          <button onClick={() => goThere()}>Go to the path</button>
-        </Flex>
-        <Flex direction={"row"}>
-          <button onClick={() => computeAndSetStatus()}>Compute Status</button>
-          <TextField
-            label=""
-            value={userStatus}
-            readOnly
-            width="400px"
-          />
-          <button onClick={() => setUserStatus('')}>Clear Status</button>
-        </Flex>
-        <Flex direction={"row"}>
-          <button onClick={() => runSimpleTest()}>Run Simple Test</button>
-          <TextField
-            label=""
-            value={simpleTestResult}
-            readOnly
-            width="60px"
-          />
-          <button onClick={() => resetLoginTracking()}>Reset Login Tracking</button>
-          <button onClick={() => addToLoginTracking()}>Add to Login Tracking</button>
-          <button onClick={() => testEmail()}>Test Email</button>
-        </Flex>
-        <ToastNotifier message={toastMessage} shouldShow={showToast} showF={setShowToast}/>
-        <Flex>
-          <button onClick={handleShowToastClick}>Show Toast</button>
-          <Button onClick={handleCreateIssueClick}> Create Issue </Button>
-          <Button onClick={handleFetchIssueClick}> Fetch Issue </Button>
-        </Flex>
+      <Flex direction="column" gap="6px">
+        <RadioGroupField
+          legend="Choose What to See"
+          name="uiChoice"
+          direction="row"
+          value={uiChoice}
+          onChange={handleUiRadioButtonChange}
+        >
+          <Radio value="uiMgmt">IssueModification UI</Radio>
+          <Radio value="randomUI">random cool stuff</Radio>
+        </RadioGroupField>
+
+        {
+          showUiMgmt &&
+          (
+            <div className="activityChoiceDiv">
+              <RadioGroupField
+                legend="What do you want to do?"
+                name="activityChoice"
+                direction="row"
+                value={activityChoice}
+                onChange={handleActivityRadioButtonChange}
+              >
+                  
+                <div className="activityButtonsDiv">
+                  <Radio value="manageUrls">Manage pro/con URLs</Radio>
+                </div>
+                <div className="activityButtonsDiv">
+                  <Radio value="manageOtherFields">Manage other Issue properties</Radio>
+                </div>
+              </RadioGroupField>
+              {
+                showOtherFieldsUi &&
+                (
+                  // UI to display if showOtherFieldsUi is true
+                  <Flex direction="column" className="otherFieldsFormDiv">
+                    <Flex direction="row">
+                      <div className="issueScrollDiv">{
+                        idClaimPairs.map(pair => (
+                          <IssueTile
+                            key={pair.issueId}
+                            issueId={pair.issueId}
+                            claim={pair.claim}
+                            selectionCallback={handleIssueTileClick} />
+                         ))}
+                      </div>
+                      <Flex direction="column" gap="0px">
+                        <div className="chosenIssueLabel">
+                          chosen Issue ID:
+                        </div>
+                        <div className="chosenIssue">
+                          <TextField
+                            label=""
+                            name=""
+                            width="240px"
+                            value={issueIdText}
+                            readOnly
+                            />
+                      </div>
+                      </Flex>
+                    </Flex>
+                    <Flex direction="row">
+                      <div className="claimLabelDiv">
+                        claim:
+                      </div>
+                      <div className="claimDiv">
+                        <TextField
+                          label=""
+                          name=""
+                          value={claimText}
+                          onChange={(e) => setClaimText(e.target.value)}
+                          />
+                      </div>
+                    </Flex>
+                    <Flex direction="row">
+                      <div className="priorityLabelDiv">
+                        priority:
+                      </div>
+                      <div className="priorityDiv">
+                        <TextField
+                          label=""
+                          name=""
+                          value={priorityText}
+                          onChange={(e) => setPriorityText(e.target.value)}
+                          />
+                      </div>
+                    </Flex>
+                    <Flex direction="row">
+                      <div className="availabilityLabel">
+                        Availability:
+                      </div>
+                      <RadioGroupField
+                        className="availabilityButtons"
+                        legend=""
+                        name="activityChoice"
+                        direction="row"
+                        value={availabilityChoice}
+                        onChange={handleAvailabilityRadioButtonChange}
+                      >
+                        <div className="availabilityButtonDiv">
+                          <Radio value="available">&nbsp;Make Available</Radio>
+                        </div>
+                        <div className="availabilityButtonDiv">
+                          <Radio value="unavailable">&nbsp;Make Unavailable</Radio>
+                        </div>
+                      </RadioGroupField>
+                      <div className="otherFieldsFormButtonsDiv">
+                        <Button className="otherFieldsFormButton" onClick={handleOtherFieldsClearButtonClick}>
+                          Clear
+                        </Button>
+                        <Button className="otherFieldsFormButton" onClick={handleOtherFieldsSubmitButtonClick}>
+                          Submit
+                        </Button>
+                      </div>
+                    </Flex>
+
+                    <div>
+
+                    </div>
+                  </Flex>
+                )
+              }
+              {
+                showUrlUi &&
+                (
+                  // UI to display if showOtherFieldsUi is true
+                  <p>This is displayed to allow modifiction of URLs.</p>
+                )
+              }
+
+            </div>
+          )
+        }
+
+        {
+          showRandom &&
+          (
+            // UI to display if showRandom is true
+            <Flex direction="column" gap="12px">
+              <Flex direction={"row"}>
+                <button onClick={() => toggleIsAdmin()}>Toggle isSuperAdmin</button>
+                value:
+                <TextField
+                  label=""
+                  value={'' + isNowSuperAdmin}
+                  readOnly
+                  placeholder="??"
+                  width="120px"
+                />
+              </Flex>
+              <Flex direction={"row"}>
+                <button onClick={() => toggleCEmail()}>{String(clearOrFill)} cEmail</button>
+                <button onClick={() => checkDbAndShowResult()}>Czech DB for corruption</button>
+                <TextField
+                  label=""
+                  value={dbCheckFeedback}
+                  readOnly
+                  width="420px"
+                />
+              </Flex>
+              <Flex direction={"row"}>
+                <button onClick={() => dispatch(setNextPath(savedPath))}>Set as next path</button>
+                <TextField
+                  label=""
+                  value={savedPath}
+                  onChange={(e) => setSavedPath(e.target.value)}
+                  width="200px"
+                />
+                <button onClick={() => goThere()}>Go to the path</button>
+              </Flex>
+              <Flex direction={"row"}>
+                <button onClick={() => computeAndSetStatus()}>Compute Status</button>
+                <TextField
+                  label=""
+                  value={userStatus}
+                  readOnly
+                  width="400px"
+                />
+                <button onClick={() => setUserStatus('')}>Clear Status</button>
+              </Flex>
+              <Flex direction={"row"}>
+                <button onClick={() => runSimpleTest()}>Run Simple Test</button>
+                <TextField
+                  label=""
+                  value={simpleTestResult}
+                  readOnly
+                  width="60px"
+                />
+                <button onClick={() => resetLoginTracking()}>Reset Login Tracking</button>
+                <button onClick={() => addToLoginTracking()}>Add to Login Tracking</button>
+                <button onClick={() => testEmail()}>Test Email</button>
+              </Flex>
+
+              <ToastNotifier message={toastMessage} shouldShow={showToast} showF={setShowToast}/>
+              <Flex>
+                <button onClick={handleShowToastClick}>Show Toast</button>
+                <Button onClick={handleCreateIssueClick}> Create Issue </Button>
+                <Button onClick={handleFetchIssueClick}> Fetch Issue </Button>
+              </Flex>
+            </Flex>
+          )
+        }
       </Flex>
     </PageWrapper>
   );
