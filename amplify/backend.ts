f20c99820b403ad43deb18a564c208a89fb77af6
Kim McCall <mccall.kim@gmail.com>
2025-10-02 00:06:58 -0400

    Restore to pre-lambda state

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,11 +3,9 @@ import { auth } from './auth/resource';
 import { data } from './data/resource';
 // import or define sendEmail
 // import { sendEmail } from './sendEmail/resource';
-import { sayHello } from './functions/say-hello/resource';
 
 defineBackend({
   auth,
   data,
   // sendEmail,
-  sayHello
 });
