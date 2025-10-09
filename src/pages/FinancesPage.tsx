da0e4baebc3d06d017438876f771f4ba2ba854e8
Kim McCall <mccall.kim@gmail.com>
2025-10-09 13:18:37 -0700

    Add FanancePage

--- src/pages/FinancesPage.tsx
+++ src/pages/FinancesPage.tsx
@@ -4,7 +4,32 @@ function FinancesPage() {
   return (
     <PageWrapper>
       <div>
-        You're on the (stub of the) Finances Page.
+        <h2>Financing this effort</h2>
+        <div>It is our intention to be utterly transparent about our expenses, our funding sources,
+          and our financial activities.</div>
+        <div>&nbsp;</div>
+        <div>At present, this is a volunteer effort ("labor of love"), and we have no ongoing personel expenses.</div>
+        <div>We may explore various funding sources, including donations and sponsorships.</div>
+        <div>&nbsp;</div>
+        <div>We've been developing on the AWS Amplify platform, and our attendant expenses for September 2025 ran $7.18.
+          We expect these to rise as we add more content and more users.</div>
+        <div>&nbsp;</div>
+        <div>So <b>by far</b> our greatest expense has been the acquisition of our domain name(s).
+          We are somewhat embarrassed to admit that we  paid $15,921.119 for truthsquad.com
+          and $4942.97 for thetruthsquad.com. These were one-time expenses, but
+          they were quite steep. We felt that these domain names were worth the cost,
+          as they are memorable and directly relevant to our mission.
+        </div>
+        <div>&nbsp;</div>
+        <div>We will be posting our monthly expenses here, as well as any donations or sponsorships we receive.
+          We will also post a summary of our annual financial activities, including a breakdown of our expenses
+          and funding sources.
+        </div>
+        <div>&nbsp;</div>
+
+        <div>If this site "takes off" at all, we anticipate begging our patrons for some help.  But for now, please enjoy it wwithout worrying about how it's paid for..</div>
+        <div>&nbsp;</div>
+        <div>Thank you for your interest and support!</div>
       </div>
     </PageWrapper>
   );
